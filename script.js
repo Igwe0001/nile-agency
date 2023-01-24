53551b724db817641506da44830d0e877774b403
Igwe0001 <nwobodoemmanuel479@gmail.com>
2023-01-24 18:30:36 +0100

    changes

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const menuButton = document.querySelector(".menu--button__container");
 const menuContainer = document.querySelector(".menu__container");
+const nav = document.querySelectorAll("nav ul li");
+const navContact = document.querySelector('#contact')
+
+window.addEventListener("scroll", () => {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 menuButton.addEventListener("click", () => {
   if (event.target.tagName == "DIV" || event.target.tagName == "SPAN") {
-    console.log("working");
     menuContainer.classList.toggle("show-nav");
   }
 });
 
-console.log(menuButton);
+navContact.addEventListener('click', () => {
+    menuContainer.classList.toggle("show-nav");
+})
+
+nav.forEach((e) => {
+  e.addEventListener("click", (el) => {
+    menuContainer.classList.remove("show-nav");
+  });
+});
