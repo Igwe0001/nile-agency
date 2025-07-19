78fcc52ff02aff4ce4767a1f9475c0e08ec296d1
Igwe0001 <nwobodoemmanuel479@gmail.com>
2025-07-19 13:02:53 +0100

    netlify mergin fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // <-- crucial for Netlify
   plugins: [react(), tailwindcss()],
 });
